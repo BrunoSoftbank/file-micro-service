40da7ffba6cd7b0b2b0a0a91c8246424e8419b06
Bruno Souza Rocha <bruno.softbank@gmail.com>
2021-01-19 10:06:34 -0300

    Adição dos modelos de arquivos de examplo disponiveis para Download

--- src/main/java/br/com/softbank/file/service/FileService.java
+++ src/main/java/br/com/softbank/file/service/FileService.java
@@ -1,6 +1,6 @@
 package br.com.softbank.file.service;
 
-
+import java.io.FileInputStream;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -23,7 +23,7 @@ import br.com.softbank.file.integration.SftpIntegration;
 
 @Service
 public class FileService {
-	
+
 	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
 	@Autowired
@@ -31,72 +31,43 @@ public class FileService {
 
 	public void upload(ResourceEnum resource, MultipartFile file) {
 		SftpSession session = null;
-		
-		if(!file.getOriginalFilename().endsWith(".xlsx") &&  !file.getOriginalFilename().endsWith(".XLSX")) {
+
+		if (!file.getOriginalFilename().endsWith(".xlsx") && !file.getOriginalFilename().endsWith(".XLSX")) {
 			throw new MediaTypeNotSupportedException(ErrosDefaultEnum.TIPO_MIDIA_NAO_SUPORTADO.getDescricao());
 		}
-		
+
 		try {
 			session = sftpIntegration.factory().getSession();
-			
+
 			StringBuilder sb = new StringBuilder("upload/");
 			sb.append(resource.getDescricao());
 			sb.append("-");
 			sb.append(LocalDate.now().format(FORMATTER));
 			sb.append(".xlsx");
-			
+
 			session.write(file.getInputStream(), sb.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if(session != null) {
+			if (session != null) {
 				session.close();
-			}			
+			}
 		}
 	}
-	
-	public void download(ResourceEnum resource,  HttpServletResponse response) throws Exception {
-		String nomeArquivo = "Modelo ".concat(resource.getDescricao().concat(".xlsx"));
-	
-		try (Workbook workbook = new XSSFWorkbook()) {
-			Sheet sheet = workbook.createSheet(nomeArquivo);
-			sheet.setDefaultColumnWidth(30);
 
-			CellStyle style = workbook.createCellStyle();
-			org.apache.poi.ss.usermodel.Font font = workbook.createFont();
-			font.setFontName("Arial");
-			font.setBold(true);
-			style.setFont(font);
+	public void download(ResourceEnum resource, HttpServletResponse response) throws Exception {
+		String arquivo = "src/main/resources/modelo/arquivos/".concat(resource.getDescricao().concat(".xlsx"));
 
-			if(resource.equals(ResourceEnum.exames)) {
-				Row header = sheet.createRow(0);
-				header.createCell(0).setCellValue("Nome");
-				header.getCell(0).setCellStyle(style);
-				header.createCell(1).setCellValue("Tipo Id");
-				header.getCell(1).setCellStyle(style);
-					
-			} else {
-				Row header = sheet.createRow(0);
-				header.createCell(0).setCellValue("Nome");
-				header.getCell(0).setCellStyle(style);
-				header.createCell(1).setCellValue("Cidade");
-				header.getCell(1).setCellStyle(style);
-				header.createCell(2).setCellValue("Bairro");
-				header.getCell(2).setCellStyle(style);
-				header.createCell(3).setCellValue("Rua");
-				header.getCell(3).setCellStyle(style);
-				header.createCell(4).setCellValue("Número");
-				header.getCell(4).setCellStyle(style);
-			}
-			
-			response.setHeader("content-disposition", "attachment; filename=" + nomeArquivo);
-			response.setContentType("application/xlsx");
+		Workbook workbook = new XSSFWorkbook(new FileInputStream(arquivo));
 
-			workbook.write(response.getOutputStream());
+		response.setHeader("content-disposition", "attachment; filename=" + resource.getDescricao().concat(".xlsx"));
+		response.setContentType("application/xlsx");
 
-			response.flushBuffer();
-			response.getOutputStream().flush();
-			response.getOutputStream().close();
-		}
+		workbook.write(response.getOutputStream());
+
+		response.flushBuffer();
+		response.getOutputStream().flush();
+		response.getOutputStream().close();
+
 	}
 }
